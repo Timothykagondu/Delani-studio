9edd7a5435ef76ae61f00466e6b213d6c06bc9c3
TimothyKagondu <timothy.2k21@gmail.com>
2022-03-14 06:49:59 +0300

    Delani

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
 
 
 
-    
+
     $(document).ready(function(){
      $('#work1').mouseover(function(){
        $('#overlay').show();
@@ -76,3 +76,29 @@ $(document).ready(function(){
     
 
   })})
+
+
+
+
+
+
+
+
+
+
+
+
+
+  $(document).ready(function(){
+    $("form#form1").submit(function(event){
+      // event.preventDefault();
+      var name = $("input#MERGE1").val();
+      var email = $("input#MERGE0").val();
+      var message = $("textarea#comment").val();
+      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      else {
+        alert("Please enter your name and email!");
+      }
+    })})
