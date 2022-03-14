da57809551f5972b564241168c1f2b6e8f8825bd
TimothyKagondu <timothy.2k21@gmail.com>
2022-03-14 03:46:07 +0300

    clearing errors

---
+++ index.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $("#development-img").click(function(){
+      $("#development-img").slideDown('1500').hide('1000');
+      $("#development").show('1500');
+    });
+    $("#development").click(function(){
+      $("#development").slideUp('1500');
+      $("#development-img").slideDown('1500');
+    });
+  });
+ 
+  $(document).ready(function(){
+    $("form#form34A").submit(function(event){
+      // event.preventDefault();
+      var name = $("input#MERGE1").val();
+      var email = $("input#MERGE0").val();
+      var message = $("textarea#comment").val();
+      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      else {
+        alert("Please enter your name and email!");
+      }
+      
+    });
+  
+  });
